deff8a240707d5632a160ddaf61c6615c7345bf7
Raymond May Jr <raymondmay@gmail.com>
2014-05-04 08:13:55 +0200

    do not require all of wb.core, check in first build

---
+++ dist/MagiConsole.js
@@ -0,0 +1,555 @@
+!function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var o;"undefined"!=typeof window?o=window:"undefined"!=typeof global?o=global:"undefined"!=typeof self&&(o=self),o.MagiConsole=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
+// shim for using process in browser
+
+var process = module.exports = {};
+
+process.nextTick = (function () {
+    var canSetImmediate = typeof window !== 'undefined'
+    && window.setImmediate;
+    var canPost = typeof window !== 'undefined'
+    && window.postMessage && window.addEventListener
+    ;
+
+    if (canSetImmediate) {
+        return function (f) { return window.setImmediate(f) };
+    }
+
+    if (canPost) {
+        var queue = [];
+        window.addEventListener('message', function (ev) {
+            var source = ev.source;
+            if ((source === window || source === null) && ev.data === 'process-tick') {
+                ev.stopPropagation();
+                if (queue.length > 0) {
+                    var fn = queue.shift();
+                    fn();
+                }
+            }
+        }, true);
+
+        return function nextTick(fn) {
+            queue.push(fn);
+            window.postMessage('process-tick', '*');
+        };
+    }
+
+    return function nextTick(fn) {
+        setTimeout(fn, 0);
+    };
+})();
+
+process.title = 'browser';
+process.browser = true;
+process.env = {};
+process.argv = [];
+
+function noop() {}
+
+process.on = noop;
+process.once = noop;
+process.off = noop;
+process.emit = noop;
+
+process.binding = function (name) {
+    throw new Error('process.binding is not supported');
+}
+
+// TODO(shtylman)
+process.cwd = function () { return '/' };
+process.chdir = function (dir) {
+    throw new Error('process.chdir is not supported');
+};
+
+},{}],2:[function(_dereq_,module,exports){
+'use strict';
+
+var inherits = _dereq_('./lib/inherits');
+var extend = _dereq_('./lib/extend');
+var clone = _dereq_('./lib/clone');
+var createUID = _dereq_('./lib/createUID');
+var fromSuper = _dereq_('./lib/fromSuper');
+
+// Self-propagating extend function.
+// Create a new class,
+// that inherits from the class found in the `this` context object.
+// This function is meant to be called,
+// in the context of a constructor function.
+function extendSelf (protoProps, staticProps) {
+  /* jshint validthis:true */
+
+  var parent = this;
+
+  protoProps = protoProps || {};
+
+  // extract mixins, if any
+  var mixins = protoProps.mixins || [];
+  delete protoProps.mixins;
+
+  // create the derived class
+  var child = inherits(parent, protoProps, staticProps);
+
+  // apply mixins to the derived class
+  var mixin;
+  while (mixins.length) {
+    mixin = mixins.shift();
+    (typeof mixin.applyToClass === 'function') &&
+      mixin.applyToClass(child);
+  }
+
+  // make the child class extensible
+  child.extend = parent.extend || extendSelf;
+  return child;
+}
+
+function WBClass (options) {
+
+  var self = this;
+
+  // Assign a unique identifier to the instance
+  self.uid = self.uid || createUID();
+
+  // save options, make sure it's at least an empty object
+  self.options = options || self.options;
+
+  // augment properties from mixins
+  self.augmentProperties();
+
+  // initialize the instance
+  self.initialize.apply(self, arguments);
+
+  // initialize all the mixins, if needed
+  // don't keep this in the initialize,
+  // initialize can be overwritten
+  self.initMixins.apply(self, arguments);
+}
+
+var proto = {
+
+  'initialize': function () {
+
+    // Return self to allow for subclass to assign
+    // super initializer value to self
+    var self = this;
+    return self;
+  },
+
+  // If any mixins were applied to the prototype, initialize them
+  'initMixins': function () {
+
+    var self = this;
+    var initializers = fromSuper.concat(self, 'initializers');
+
+    var initializer;
+    while (initializers.length) {
+      initializer = initializers.shift();
+      (typeof initializer === 'function') &&
+        initializer.apply(self, arguments);
+    }
+  },
+
+  // If any proerties were defined in the mixins, augment them to the instance
+  'augmentProperties': function () {
+
+    var self = this;
+    var properties = fromSuper.merge(self, 'properties');
+
+    function augmentProperty (property, value) {
+
+      var type = typeof value;
+
+      if (type === 'function') {
+        self[property] = value.call(self);
+      }
+      else if (type === 'object') {
+        self[property] = clone(value, true);
+      }
+      else {
+        self[property] = value;
+      }
+    }
+
+    for (var key in properties) {
+      augmentProperty(key, properties[key]);
+    }
+  }
+};
+
+extend(WBClass.prototype, proto);
+WBClass.extend = extendSelf;
+
+module.exports = WBClass;
+
+},{"./lib/clone":4,"./lib/createUID":5,"./lib/extend":6,"./lib/fromSuper":7,"./lib/inherits":9}],3:[function(_dereq_,module,exports){
+'use strict';
+
+function assert (condition, message) {
+  if (!condition) {
+    throw new Error(message || '');
+  }
+}
+
+var nativeIsArray = Array.isArray;
+assert.empty = function (object, message) {
+  var keys = nativeIsArray(object) ? object : Object.keys(object);
+  assert(keys.length === 0, message);
+};
+
+assert.array = function (array, message) {
+  assert(nativeIsArray(array), message);
+};
+
+assert.class = function (klass, message) {
+  var proto = klass.prototype;
+  assert(proto && proto.constructor === klass, message);
+};
+
+var types = [
+  'undefined',
+  'boolean',
+  'number',
+  'string',
+  'function',
+  'object'
+];
+
+function typecheck (type) {
+  assert[type] = function (o, message) {
+    assert(typeof o === type, message);
+  };
+}
+
+while (types.length) {
+  typecheck(types.shift());
+}
+
+module.exports = assert;
+},{}],4:[function(_dereq_,module,exports){
+'use strict';
+
+var nativeIsArray = Array.isArray;
+
+function cloneArray (arr, isDeep) {
+  arr = arr.slice();
+  if (isDeep) {
+    var newArr = [], value;
+    while (arr.length) {
+      value = arr.shift();
+      value = (value instanceof Object) ? clone(value, isDeep) : value;
+      newArr.push(value);
+    }
+    arr = newArr;
+  }
+  return arr;
+}
+
+function cloneDate (date) {
+  return new Date(date.getTime());
+}
+
+function cloneObject (source, isDeep) {
+  var object = {};
+  for (var key in source) {
+    if (source.hasOwnProperty(key)) {
+      var value = source[key];
+      if (value instanceof Date) {
+        object[key] = cloneDate(value);
+      } else if (typeof value === 'object' && value !== null && isDeep) {
+        object[key] = clone(value, isDeep);
+      } else {
+        object[key] = value;
+      }
+    }
+  }
+  return object;
+}
+
+function clone (obj, isDeep) {
+
+  if (nativeIsArray(obj)) {
+    return cloneArray(obj, isDeep);
+  }
+
+  return cloneObject(obj, isDeep);
+}
+
+module.exports = clone;
+
+},{}],5:[function(_dereq_,module,exports){
+'use strict';
+
+function replacer (match) {
+  var rand = Math.random() * 16 | 0;
+  var chr = (match === 'x') ? rand : (rand & 0x3 | 0x8);
+  return chr.toString(16);
+}
+
+function createUID (prefix) {
+  var uid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, replacer);
+  return String(!prefix ? '' : prefix) + uid;
+}
+
+module.exports = createUID;
+
+},{}],6:[function(_dereq_,module,exports){
+'use strict';
+
+var toArray = _dereq_('./toArray');
+var merge = _dereq_('./merge');
+var assert = _dereq_('./assert');
+
+function extend () {
+
+  // convert the argument list into an array
+  var args = toArray(arguments);
+
+  // validate input
+  assert(args.length > 0, 'extend expect one or more objects');
+
+  // loop through the arguments
+  // & merging them recursively
+  var object = args.shift();
+  while (args.length) {
+    merge(object, args.shift());
+  }
+
+  return object;
+}
+
+module.exports = extend;
+
+},{"./assert":3,"./merge":10,"./toArray":11}],7:[function(_dereq_,module,exports){
+'use strict';
+
+var merge = _dereq_('./merge');
+var extend = _dereq_('./extend');
+
+function mergeFromSuper (instance, key) {
+
+  var constructor = instance.constructor;
+  var proto = constructor.prototype;
+
+  var baseData = {};
+  if (instance.hasOwnProperty(key)) {
+    baseData = instance[key];
+  } else if (proto.hasOwnProperty(key)) {
+    baseData = proto[key];
+  }
+
+  var _super = constructor && constructor.__super__;
+  if (_super) {
+    baseData = merge(mergeFromSuper(_super, key), baseData);
+  }
+
+  return extend({}, baseData);
+}
+
+function concatFromSuper (instance, key) {
+
+  var constructor = instance.constructor;
+  var proto = constructor.prototype;
+
+  var baseData = [];
+  if (instance.hasOwnProperty(key)) {
+    baseData = instance[key];
+  } else if (proto.hasOwnProperty(key)) {
+    baseData = proto[key];
+  }
+
+  var _super = constructor && constructor.__super__;
+  if (_super) {
+    baseData = [].concat(concatFromSuper(_super, key), baseData);
+  }
+
+  return [].concat(baseData);
+}
+
+module.exports = {
+  'merge': mergeFromSuper,
+  'concat': concatFromSuper
+};
+
+},{"./extend":6,"./merge":10}],8:[function(_dereq_,module,exports){
+'use strict';
+
+function functions (obj) {
+  var funcs = [];
+  for (var key in obj) {
+    if (typeof obj[key] === 'function') {
+      funcs.push(key);
+    }
+  }
+  return funcs;
+}
+
+module.exports = functions;
+
+},{}],9:[function(_dereq_,module,exports){
+'use strict';
+
+var extend = _dereq_('./extend');
+
+// Helper function to correctly set up the prototype chain, for subclasses.
+// Similar to `goog.inherits`, but uses a hash of prototype properties and
+// class properties to be extended.
+function inherits (parent, protoProps, staticProps) {
+
+  var child;
+
+  // The constructor function for the new subclass is either defined by you
+  // (the "constructor" property in your `extend` definition), or defaulted
+  // by us to simply call `super()`.
+  if (protoProps && protoProps.hasOwnProperty('constructor')) {
+    child = protoProps.constructor;
+  }
+  else {
+    child = function () {
+      return parent.apply(this, arguments);
+    };
+  }
+
+  // Inherit class (static) properties from parent.
+  extend(child, parent);
+
+  // Set the prototype chain to inherit from `parent`, without calling
+  // `parent`'s constructor function.
+  child.prototype = Object.create(parent.prototype);
+
+  // Add prototype properties (instance properties) to the subclass,
+  // if supplied.
+  extend(child.prototype, protoProps);
+
+  // Correctly set child's `prototype.constructor`.
+  child.prototype.constructor = child;
+
+  // Add static properties to the constructor function, if supplied.
+  extend(child, staticProps);
+
+  // Set a convenience property
+  // in case the parent's prototype is needed later.
+  child.__super__ = parent.prototype;
+
+  return child;
+}
+
+module.exports = inherits;
+
+},{"./extend":6}],10:[function(_dereq_,module,exports){
+'use strict';
+
+var toArray = _dereq_('./toArray');
+
+function merge (object, source) {
+  var sources = toArray(arguments, 1);
+  while (sources.length) {
+    source = sources.shift();
+    for (var key in source) {
+      if (source.hasOwnProperty(key)) {
+        object[key] = source[key];
+      }
+    }
+  }
+  return object;
+}
+
+module.exports = merge;
+
+},{"./toArray":11}],11:[function(_dereq_,module,exports){
+'use strict';
+
+var slice = Array.prototype.slice;
+function toArray (obj, skip) {
+  return slice.call(obj, skip || 0);
+}
+
+module.exports = toArray;
+
+},{}],12:[function(_dereq_,module,exports){
+(function (process,global){
+'use strict';
+
+var Console = global.console;
+
+var WBClass = _dereq_('../node_modules/wunderbits.core/public/WBClass');
+var assert =  _dereq_('../node_modules/wunderbits.core/public/lib/assert');
+var functions =  _dereq_('../node_modules/wunderbits.core/public/lib/functions');
+
+var MagiConsolePrototype = {
+
+  'constructor': function (namespace) {
+
+    var self = this;
+
+    assert.string(namespace, 'namespace must be a string');
+
+    // if a cached namespaced logger already exists, simply return it
+    var namespaceMap = MagiConsole.namespaces;
+    if (namespaceMap[namespace] instanceof MagiConsole) {
+      return namespaceMap[namespace];
+    }
+
+    self.namespace = namespace;
+    namespaceMap[namespace] = self;
+
+    WBClass.call(self);
+
+    // forces GC of potentially large object immediately
+    namespaceMap = null;
+  },
+
+  'shouldRun': function (method) {
+
+    var pattern = MagiConsole.pattern;
+    var level = MagiConsole.level;
+
+    var shouldRun = pattern && pattern.test(this.namespace);
+    shouldRun = shouldRun && (level ? method === level : true);
+    return !!(shouldRun && Console);
+  }
+};
+
+functions(Console).forEach(function (method) {
+
+  MagiConsolePrototype[method] = function () {
+
+    this.shouldRun(method) && Console[method].apply(Console, arguments);
+  };
+});
+
+var MagiConsole = WBClass.extend(MagiConsolePrototype);
+
+MagiConsole.namespaces = {};
+MagiConsole.pattern = undefined;
+MagiConsole.level = undefined;
+
+MagiConsole.release = function () {
+
+  MagiConsole.namespaces = {};
+};
+
+MagiConsole.log = function (regexPatternString) {
+
+  assert.string(regexPatternString, 'regexPatternString must be a string');
+  regexPatternString = regexPatternString === '*' ? '.?' : regexPatternString;
+  MagiConsole.pattern = new RegExp(regexPatternString);
+};
+
+MagiConsole.setLevel = function (logLevel) {
+
+  assert.string(logLevel, 'logLevel must be a string');
+  logLevel = logLevel === '*' ? undefined : logLevel;
+  MagiConsole.level = logLevel;
+};
+
+if (!process.browser) {
+  var envPattern = process.env.MLOG;
+  var envLevel = process.env.MLEVEL;
+  envPattern && MagiConsole.log(envPattern);
+  envLevel && MagiConsole.setLevel(envLevel);
+}
+
+module.exports = global.MagiConsole = MagiConsole;
+}).call(this,_dereq_("/Users/test/Documents/6wunderkinder/WBLogger/node_modules/gulp-cjs/node_modules/browserify/node_modules/insert-module-globals/node_modules/process/browser.js"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{"../node_modules/wunderbits.core/public/WBClass":2,"../node_modules/wunderbits.core/public/lib/assert":3,"../node_modules/wunderbits.core/public/lib/functions":8,"/Users/test/Documents/6wunderkinder/WBLogger/node_modules/gulp-cjs/node_modules/browserify/node_modules/insert-module-globals/node_modules/process/browser.js":1}]},{},[12])
+//@ sourceMappingURL=MagiConsole.map
+(12)
+});
